59e4beb9afcfd72f71d5d929991d15dcce472da3
Hector Romano <tulio.hector@gmail.com>
2022-12-14 14:35:41 -0300

    add autogenerate sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://romanohector.vercel.app/',
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 5000
+  }
