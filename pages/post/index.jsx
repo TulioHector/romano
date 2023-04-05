eb5fac0a4acdf0e1aaedf37004ab637e7755b27e
Hector Abraham Romano <tulio.hector@gmail.com>
2023-04-05 19:37:41 -0300

    change view list posts like masonry

--- pages/post/index.jsx
+++ pages/post/index.jsx
@@ -23,9 +23,9 @@ class Home extends PureComponent {
     render() {
         return this.state.hasMounted && (
             <>
-                <div className="container px-4 px-lg-5">
-                    <div className="row gx-4 gx-lg-5 justify-content-center">
-                        <div className="col-md-10 col-lg-8 col-xl-7" id='poastList'>
+                <div className="mb-4">
+                    <div className="container">
+                        <div className="row" id='poastList'>
                             <List />
                         </div>
                     </div>
--- pages/post/list.jsx
+++ pages/post/list.jsx
@@ -1,18 +1,16 @@
 "use client"
 import { PureComponent, Fragment, createRef } from "react";
+import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import { PageContext } from '../../components/context';
 import Database from '../../components/Firebase';
 import moment from 'moment';
-
-const InfiniteScroll = dynamic(() => import('react-infinite-scroll-component'), {
-    ssr: false,
-  });
+//import Masonry from 'react-masonry-css';
 
 class List extends PureComponent {
     static contextType = PageContext;
     page = createRef(1);
-    
+
     constructor(props) {
         super(props);
         this.state = {
@@ -26,7 +24,7 @@ class List extends PureComponent {
 
     async loadPosts(page) {
         let info = [];
-        if(page){
+        if (page) {
             const list = await Database.getPostList();
             list.forEach((element) => {
                 const data = element.data();
@@ -36,10 +34,15 @@ class List extends PureComponent {
         return info;
     }
 
+    cargarMasArticulos = () => {
+        //setNumArticulos(numArticulos + 10);
+        console.log("voy a buscar mas posts");
+    };
+
     async componentDidMount() {
         try {
             const language = this.state.currentLocale && window.navigator.userLanguage || window.navigator.language || navigator.language;
-            this.setState({ hasMounted: true, currentLocale: language});
+            this.setState({ hasMounted: true, currentLocale: language });
             const Fetchdata = async () => {
                 const info = await this.loadPosts(this.page);
                 const listPost = this.state.posts.concat(info)
@@ -53,35 +56,46 @@ class List extends PureComponent {
     render() {
         return this.state.hasMounted && (
             <>
-                <InfiniteScroll
-                    dataLength={3}
-                    next={this.loadPosts.bind(this)}
-                    hasMore={true}
-                    loader={<h4>Loading...</h4>}
-                    endMessage={
-                        <p style={{ textAlign: 'center' }}>
-                          <b>Yay! You have seen it all</b>
-                        </p>
-                      }
-                >
-                    {this.state.posts.map(item => (
-                        <Fragment key={item.id}>
-                            <div className="post-preview" id={item.id}>
-                                <a href={`${this.state.currentLocale}/${item.url}/${item.id}`}>
-                                    <h2 className="post-title">{item.Title}</h2>
-                                    <h3 className="post-subtitle">{item.Description}</h3>
-                                </a>
+                {this.state.posts.map(item => (
+                    // <Fragment key={item.id}>
+                    //     <div className="post-preview" id={item.id}>
+                    //         <a href={`${this.state.currentLocale}/${item.url}/${item.id}`}>
+                    //             <h2 className="post-title">{item.Title}</h2>
+                    //             <h3 className="post-subtitle">{item.Description}</h3>
+                    //         </a>
 
-                                <p className="post-meta">
-                                    Posted by
-                                    <a href="#!"> {item.Author} </a>
-                                    {moment(item.DatePublish.toDate()).format('LL')}
-                                </p>
+                    //         <p className="post-meta">
+                    //             Posted by
+                    //             <a href="#!"> {item.Author} </a>
+                    //             {moment(item.DatePublish.toDate()).format('LL')}
+                    //         </p>
+                    //     </div>
+                    //     <hr className="my-4" />
+                    // </Fragment>
+
+                    <Fragment key={item.id}>
+                        <div className="col-sm-6 col-lg-4 mb-4">
+                            <div className="card grid-item border-light mb-3 card-posts text-right" key={item.id} style={{ "width": "511", "height": "260" }}>
+                                <div className="card-img-block">
+                                    <Image src={`/assets/posts/${item.id}/${item.cover}`} alt={item.Title} className="card-img-top" width={511} height={260} />
+                                </div>
+                                <div className="card-body">
+                                    <a href={`${this.state.currentLocale}/${item.url}/${item.id}`}>
+                                        <h5 className="card-title">{item.Title}</h5>
+                                    </a>
+
+                                    <p className="card-text">{item.Description}</p>
+                                    <p className="card-text"><small className="text-muted">{moment(item.DatePublish.toDate()).format('LL')}</small></p>
+                                </div>
                             </div>
-                            <hr className="my-4" />
-                        </Fragment>
-                    ))}
-                </InfiniteScroll>
+                        </div>
+
+                    </Fragment>
+                ))}
+
+                {(
+                    <button onClick={this.cargarMasArticulos}>Cargar más artículos</button>
+                )}
             </>
         );
     }
